b0bdd6cfdf1658d6f0a112078874812272644c5b
careyang <mastercy919@hotmail.com>
2022-06-07 17:15:50 +0800

    color 优化

--- CYCategories.podspec
+++ CYCategories.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'CYCategories'
-  s.version          = '0.2.0'
+  s.version          = '0.4.0'
   s.summary          = '开发常用的扩展'
 
 # This description is used to generate tags and improve search results.
--- CYCategories/Classes/ColorExtension.swift
+++ CYCategories/Classes/ColorExtension.swift
@@ -8,16 +8,7 @@ extension UIColor {
         self.init(red: r / 255.0, green: g / 255.0, blue: b / 255.0, alpha: a)
     }
 
-
-    private convenience init?(hex: Int, alpha: CGFloat = 1.0) {
-        self.init(red: CGFloat( (hex & 0xFF0000) >> 16 ) / 255.0,
-                  green: CGFloat( (hex & 0x00FF00) >> 8 ) / 255.0,
-                  blue: CGFloat( (hex & 0x0000FF) >> 0 ) / 255.0,
-                  alpha: alpha
-        )
-    }
-
-    convenience init?(red: Int, green: Int, blue: Int, transparency: CGFloat = 1) {
+    public convenience init?(red: Int, green: Int, blue: Int, transparency: CGFloat = 1) {
         guard red >= 0 && red <= 255 else { return nil }
         guard green >= 0 && green <= 255 else { return nil }
         guard blue >= 0 && blue <= 255 else { return nil }
@@ -29,7 +20,7 @@ extension UIColor {
         self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: trans)
     }
 
-    convenience init?(hex: Int, transparency: CGFloat = 1) {
+    public convenience init?(hex: Int, transparency: CGFloat = 1) {
         var trans = transparency
         if trans < 0 { trans = 0 }
         if trans > 1 { trans = 1 }
@@ -40,7 +31,7 @@ extension UIColor {
         self.init(red: red, green: green, blue: blue, transparency: trans)
     }
 
-    convenience init?(hexString: String, transparency: CGFloat = 1) {
+    public convenience init?(hexString: String, transparency: CGFloat = 1) {
         var string = ""
         if hexString.lowercased().hasPrefix("0x") {
             string =  hexString.replacingOccurrences(of: "0x", with: "")
@@ -70,15 +61,15 @@ extension UIColor {
 
 
     // 16进制
-    func toHexString() -> String {
+    public func toHexString() -> String {
         var r: CGFloat = 0
         var g: CGFloat = 0
         var b: CGFloat = 0
         var a: CGFloat = 0
         getRed(&r, green: &g, blue: &b, alpha: &a)
         let rgb: Int = (Int)(r * 255) << 16 |
-                       (Int)(g * 255) << 8  |
-                       (Int)(b * 255) << 0
+        (Int)(g * 255) << 8  |
+        (Int)(b * 255) << 0
 
         return String(format:"#%06x", rgb)
     }
